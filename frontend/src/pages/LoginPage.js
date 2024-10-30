83840d05099da67f61dc4c512cb80a0897870bc1
SPIDY123456 <73572033+SPIDY123456@users.noreply.github.com>
2024-10-30 17:47:55 +0530

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = `http://localhost:5000`; 
+const API_URL = `https://redditclone-ijh8.onrender.com`; 
 
 const Login = () => {
     const [nickname, setNickname] = useState('');
